ec8244bc5b71d57aefce9894079a9a6b4c0dfd30
unknown <nicolasawelsh@gmail.com>
2023-10-31 23:01:55 -0400

    both use cases functional, not clean

--- time_histogram.py
+++ time_histogram.py
@@ -2,42 +2,55 @@ import argparse
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
-from datetime import datetime, timedelta
+from datetime import datetime, timedelta, timezone
 from dateutil import parser
-import re
 import csv
+import pytz
+import re
 
-def is_pst_pdt_format(time_str):
-    pst_pdt_pattern = re.compile(r"\d{4}-\d{2}-\d{2} \d{2}:\d{2}:\d{2} [A-Za-z]+")
-    return bool(pst_pdt_pattern.match(time_str))
 
-def convert_to_utc(time_str):
-    if "PST" in time_str:
-        offset = -8
-    elif "PDT" in time_str:
-        offset = -7
-    elif "GMT" in time_str:
-        offset = 0
+# Custom tzinfos dict
+tzinfos = {
+    "PST": pytz.FixedOffset(-8*60),  # UTC-8 for PST
+    "PDT": pytz.FixedOffset(-7*60),  # UTC-7 for PDT
+    "MST": pytz.FixedOffset(-7*60),  # UTC-7 for MST
+    "GMT": pytz.UTC,                 # UTC for GMT
+    "UTC": pytz.UTC,                 # UTC for UTC
+}
+
+
+def parse_datetime(time_str, autopsy_deleted):
+    if re.match(r"\d{4}-\d{2}-\d{2} \d{2}:\d{2}:\d{2}.\d+", time_str) or re.match(r"\d{4}-\d{2}-\d{2} \d{2}:\d{2}:\d{2} [A-Za-z]+", time_str):
+        try:
+            parsed_time = parser.parse(time_str, tzinfos=tzinfos)
+            if autopsy_deleted:
+                # Check if the parsed datetime is offset-naive (no timezone info)
+                if not parsed_time.tzinfo:
+                    parsed_time = parsed_time.replace(tzinfo=pytz.UTC)
+                else:
+                    parsed_time = parsed_time.astimezone(pytz.UTC)
+            return parsed_time
+        except ValueError:
+            # Handle invalid or unexpected timestamp format
+            return None
     else:
-        raise ValueError("Invalid timezone")
-    time_obj = datetime.strptime(re.sub(r'[A-Za-z]+$', '', time_str), "%Y-%m-%d %H:%M:%S")
-    return time_obj - timedelta(hours=offset)
-
-def parse_datetime(time_str):
-    try:
-        return parser.parse(time_str)
-    except ValueError:
-        # Handle invalid or unexpected timestamp format
         return None
 
-def generate_histogram(data, start_time, end_time, bucket_size):
-    start_time = datetime.strptime(start_time, "%Y-%m-%d %H:%M:%S")
-    end_time = datetime.strptime(end_time, "%Y-%m-%d %H:%M:%S")
+
+def generate_histogram(data, start_time, end_time, bucket_size, autopsy_deleted):
+    if autopsy_deleted:
+        start_time = parser.parse(start_time, tzinfos=tzinfos).astimezone(pytz.UTC)
+        end_time = parser.parse(end_time, tzinfos=tzinfos).astimezone(pytz.UTC)
+    else:
+        start_time = parser.parse(start_time)
+        end_time = parser.parse(end_time)
     num_buckets = int((end_time - start_time).total_seconds() / (bucket_size * 60))
     bins = [start_time + timedelta(minutes=i * bucket_size) for i in range(num_buckets + 1)]
     
     hist, bins = np.histogram(data, bins)
+
     return hist, bins
+
 
 def save_to_csv(hist, bins, file_name):
     with open(file_name, 'w', newline='') as csvfile:
@@ -46,6 +59,7 @@ def save_to_csv(hist, bins, file_name):
         for i in range(len(bins) - 1):
             writer.writerow([bins[i], bins[i + 1], hist[i]])
 
+
 def main():
     parser = argparse.ArgumentParser(description="Generate a time histogram from a CSV file.")
     parser.add_argument("-f", "--file", help="Path to the CSV file", required=True)
@@ -53,14 +67,16 @@ def main():
     parser.add_argument("-b", "--bucket_size", help="Bucket size in minutes", type=int, required=True)
     parser.add_argument("-s", "--start_time", help="Start time in the format '%Y-%m-%d %H:%M:%S'", required=True)
     parser.add_argument("-e", "--end_time", help="End time in the format '%Y-%m-%d %H:%M:%S'", required=True)
+    parser.add_argument("-a", "--autopsy_deleted", action='store_true')
     args = parser.parse_args()
 
     df = pd.read_csv(args.file, usecols=[args.column])
     time_data = df[args.column].tolist()
 
-    time_data = [convert_to_utc(time_str) if is_pst_pdt_format(time_str) else parse_datetime(time_str) for time_str in time_data]
+    time_data = [parse_datetime(time_str, args.autopsy_deleted) if isinstance(time_str, str) else None for time_str in time_data]
+    #print(time_data)
     time_data = [time for time in time_data if time is not None]
-    hist, bins = generate_histogram(time_data, args.start_time, args.end_time, args.bucket_size)
+    hist, bins = generate_histogram(time_data, args.start_time, args.end_time, args.bucket_size, args.autopsy_deleted)
 
     # Save histogram data to CSV file
     save_to_csv(hist, bins, "hist.csv")
